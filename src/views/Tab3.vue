69db9f6d11225aba0d1df5fcc9886887bafad2c5
Ryan Li <1078054388@qq.com>
2018-11-01 20:03:00 +0800

    新测试

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -2,21 +2,21 @@
     <div>
         <Row>
             <Col span="6">
-                <Row>
-                    <Col span="24" class="search-input">
-                        <Input v-model="origin" placeholder="出发地"/>
-                    </Col>
-                    <Col span="24" class="search-input">
-                        <Input v-model="target" search enter-button="出发" placeholder="目的地" @on-search="go"/>
-                    </Col>
-                </Row>
+            <Row>
+                <Col span="24" class="search-input">
+                <Input v-model="origin" placeholder="出发地" />
+                </Col>
+                <Col span="24" class="search-input">
+                <Input v-model="target" search enter-button="出发" placeholder="目的地" @on-search="go" />
+                </Col>
+            </Row>
             </Col>
         </Row>
         <br>
         <Row>
             <Col span="24" class="search-input">
-                <p v-if="origin && target">你输入的是: {{ origin + "和" + target }}</p>
-                <p v-else>你为什么要清空{{origin ? "目的地" : (target ? "出发地" : "出发地和目的地")}}</p>
+            <p v-if="origin && target">你输入的是: {{ origin + "和" + target }}</p>
+            <p v-else>你为什么要清空{{origin ? "目的地" : (target ? "出发地" : "出发地和目的地")}}</p>
             </Col>
         </Row>
     </div>
@@ -37,15 +37,14 @@ export default {
     methods: {
         drawLine() {},
         go() {
-            if (this.origin && this.target){
+            if (this.origin && this.target) {
                 this.$Notice.open({
-                    title: '你出发啦！',
+                    title: "你出发啦！",
                     desc: this.origin + "->" + this.target
                 });
             } else {
                 alert("请输入出发地和目的地");
             }
-            
         }
     }
 };
